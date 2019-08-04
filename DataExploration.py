d4bfde77fb2057c718f4e07c4d3dcb9edb9f9528
alex <casanas10@gmail.com>
2019-08-03 20:54:08 -0400

    Merged both news and apple OLHCV datasets

--- DataExploration.py
+++ DataExploration.py
@@ -1,9 +1,13 @@
 import os
 import glob
 import re
+import numpy as np
+import pandas as pd
 
+import spacy
+nlp = spacy.load('en_core_web_sm')
 
-def get_data(folder_path):
+def get_news_data(folder_path):
     # Load the dataset
     dates = []
     headlines = []
@@ -32,8 +36,62 @@ def get_data(folder_path):
 
     return dates, headlines, articles, urls
 
+def get_apple_news_data(data_df):
+
+    if not os.path.exists('data/apple_news.pkl'):
+        apple_news_df = pd.DataFrame(columns=['date', 'headline', 'text', 'url'])
+
+        for i, row in enumerate(data_df.values):
+            doc = nlp(row[2])
+            # set_of_orgs = set()
+            for X in doc.ents:
+                if X.label_ == 'ORG':
+                    if X.text == 'Apple' or X.text == "APPL":
+                        # set_of_orgs.add(X.text)
+                        print(X.text, X.label_)
+                        print(row[0])
+                        apple_news_df.loc[i] = row
+                        break
+
+        apple_news_df.to_pickle("./data/apple_news.pkl")
+
+def cleanup_date_format(x):
+    print(x)
+
 
 if __name__ == "__main__":
-    folder_path = 'data/ReutersNews106521'
-    dates, headlines, articles, urls = get_data(folder_path)
-    print(dates[0])
+
+    if not os.path.exists('data/newspickle.pkl'):
+        folder_path = 'data/ReutersNews106521'
+        dates, headlines, articles, urls = get_news_data(folder_path)
+
+        date_array = np.array(dates)
+        headline_array = np.array(headlines)
+        text_array = np.array(articles)
+        urls_array = np.array(urls)
+
+        print(date_array.shape)
+        print(headline_array.shape)
+        print(text_array.shape)
+        print(urls_array.shape)
+
+        all_data_df = pd.DataFrame({
+            'date': date_array,
+            'headline': headline_array,
+            'text': text_array,
+            'url': urls_array
+        })
+
+        all_data_df.to_pickle("./data/newspickle.pkl")
+
+
+    all_data_df = pd.read_pickle("./data/newspickle.pkl")
+
+    apple_news_df = pd.read_pickle("./data/apple_news.pkl")
+
+    apple_news = apple_news_df['Date'].apply(lambda x: cleanup_date_format(x))
+            # for line in headlines[0:5]:
+    #     print(line)
+        # doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
+        # print([(X.text, X.label_) for X in doc.ents])
+
